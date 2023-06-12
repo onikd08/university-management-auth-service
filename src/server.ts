0b9856c9007a40e8f912ca5988d6a69d44b1ba2d
Anik Das <onikd08@gmail.com>
2023-06-12 06:54:35 +0300

    Handle SIGTERM

--- src/server.ts
+++ src/server.ts
@@ -4,19 +4,26 @@ import config from './config/index'
 import { logger, errorLogger } from './shared/logger'
 import { Server } from 'http'
 
+let server: Server
 /*
 Uncaught exception handler:
 As the main() function is async, we have to handle uncaught 
 exceptions after the main() function call
 */
 
-process.on('uncaughtException', () => {
-  console.log('Uncaught exception detected')
+process.on('uncaughtException', error => {
+  errorLogger.error(error)
   process.exit(1)
 })
 
+// SIGTERM
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM is received')
+  if (server) {
+    server.close()
+  }
+})
 async function main() {
-  let server: Server
   try {
     await mongoose.connect(config.database_url as string)
     logger.info('Database connection successful')
@@ -28,7 +35,7 @@ async function main() {
   }
 
   process.on('unhandledRejection', error => {
-    console.log('Unhandled error detected, closing server')
+    logger.info('Unhandled error detected, closing server')
     if (server) {
       server.close(() => {
         errorLogger.error(error)
