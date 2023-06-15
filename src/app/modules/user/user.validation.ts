e9f64bf42f2843d9b578168bdf4d1bed13de1e39
Anik Das <onikd08@gmail.com>
2023-06-15 12:14:19 +0300

    Fix zod validation

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -2,10 +2,12 @@ import { z } from 'zod';
 
 const createUserZodSchema = z.object({
   body: z.object({
-    role: z.string({
-      required_error: `Field 'role' is required`,
+    user: z.object({
+      role: z.string({
+        required_error: `Field 'role' is required`,
+      }),
+      password: z.string().optional(),
     }),
-    password: z.string().optional(),
   }),
 });
 
