dd96f6ba4157caaa0c353aec9030bf7e9954115a
Anik Das <onikd08@gmail.com>
2023-06-15 12:14:48 +0300

    Optimize routes

---
+++ src/app/routes/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import { UserRoutes } from '../modules/user/user.route';
+import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.route';
+
+const router = express.Router();
+
+const moduleRoutes = [
+  {
+    path: '/users',
+    route: UserRoutes,
+  },
+  {
+    path: '/academic-semesters',
+    route: AcademicSemesterRoutes,
+  },
+];
+
+moduleRoutes.forEach(item => {
+  router.use(item.path, item.route);
+});
+
+export default router;
