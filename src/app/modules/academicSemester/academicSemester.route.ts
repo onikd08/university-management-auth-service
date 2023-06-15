7f35c5f4bf3dbaab3d03fa4c2cc38be6da93592c
Anik Das <onikd08@gmail.com>
2023-06-15 11:22:00 +0300

    Create Semester code validation in service layer

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { AcademicSemesterValidation } from './academicSemester.validation';
-import { AcademicSemesterController } from './academicSemester.Controller';
+import { AcademicSemesterController } from './academicSemester.controller';
 
 const router = express.Router();
 
