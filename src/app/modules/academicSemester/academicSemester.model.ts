a3738fae8f4255c822074e13a58fdfa15a9f51b2
Anik Das <onikd08@gmail.com>
2023-06-15 10:19:32 +0300

    Handle same year and same semester validation

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -8,22 +8,55 @@ import {
   academicSemesterMonths,
   academicSemesterTitles,
 } from './academicSemester.constant';
+import ApiError from '../../../errors/ApiError';
+
+import httpStatus from 'http-status';
 
 const academicSemesterSchema = new Schema<
   IAcademicSemester,
   AcademicSemesterModel
 >(
   {
-    title: { type: String, required: true, enum: academicSemesterTitles },
-    year: { type: Number, required: true },
-    code: { type: String, required: true, enum: academicSemesterCodes },
-    startMonth: { type: String, required: true, enum: academicSemesterMonths },
-    endMonth: { type: String, required: true, enum: academicSemesterMonths },
+    title: {
+      type: String,
+      required: true,
+      enum: academicSemesterTitles,
+    },
+    year: {
+      type: Number,
+      required: true,
+    },
+    code: {
+      type: String,
+      required: true,
+      enum: academicSemesterCodes,
+    },
+    startMonth: {
+      type: String,
+      required: true,
+      enum: academicSemesterMonths,
+    },
+    endMonth: {
+      type: String,
+      required: true,
+      enum: academicSemesterMonths,
+    },
   },
   {
     timestamps: true,
   }
 );
+
+academicSemesterSchema.pre('save', async function (next) {
+  const isExist = await AcademicSemester.findOne({
+    title: this.title,
+    year: this.year,
+  });
+  if (isExist) {
+    throw new ApiError(httpStatus.CONFLICT, 'Academic Semester Already Exist');
+  }
+  next();
+});
 
 export const AcademicSemester = model<IAcademicSemester, AcademicSemesterModel>(
   'AcademicSemester',
