9f85a93c8001a9768120b1042f31fc5d84943aab
Anik Das <onikd08@gmail.com>
2023-07-05 07:41:54 +0300

    Populate acadmicFaculty field in acadmemicDepartment

--- src/app/modules/academicDepartment/academicDepartment.service.ts
+++ src/app/modules/academicDepartment/academicDepartment.service.ts
@@ -2,21 +2,25 @@ import { IAcademicDepartment } from './academicDepartment.interface';
 import { AcademicDepartment } from './academicDepartment.model';
 
 const getAllDepartments = async (): Promise<IAcademicDepartment[]> => {
-  const result = await AcademicDepartment.find({});
+  const result = await AcademicDepartment.find({}).populate('academicFaculty');
   return result;
 };
 
 const createDepartment = async (
   payload: IAcademicDepartment
 ): Promise<IAcademicDepartment> => {
-  const result = await AcademicDepartment.create(payload);
+  const result = (await AcademicDepartment.create(payload)).populate(
+    'academicFaculty'
+  );
   return result;
 };
 
 const getSingleDepartment = async (
   id: string
 ): Promise<IAcademicDepartment | null> => {
-  const result = await AcademicDepartment.findById(id);
+  const result = await AcademicDepartment.findById(id).populate(
+    'academicFaculty'
+  );
   return result;
 };
 
@@ -35,7 +39,8 @@ const updateDepartment = async (
     { _id: id },
     payload,
     { new: true }
-  );
+  ).populate('academicFaculty');
+  console.log(result);
   return result;
 };
 
